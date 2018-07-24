8fca27bc910aee621afc47e2c316f38673024e8d
xwp <xwp_fullstack@163.com>
2018-07-24 22:09:15 +0800

    bbb

--- information_web/config.py
+++ information_web/config.py
@@ -6,7 +6,7 @@ import logging
 class Config(object):
     # DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_web'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@172.24.253.107:3306/information_web'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     REDIS_HOST = '127.0.0.1'
